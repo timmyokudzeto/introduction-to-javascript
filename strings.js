a0af7ef09e125750e44e49351f41d1ef334048a4
timmyokudzeto <timiokudzeto@gmail.com>
2018-08-11 14:39:38 +0100

    my first javascript project

---
+++ strings.js
@@ -0,0 +1,11 @@
+
+var myname = "timi"
+myname = "hin"
+myname = "tayo"
+console.log(myname)
+var multiply = 3*2
+console.log(multiply)
+var sentence = "timi lives in"
+var listofaddresses = ["lagos","abuja","ogba"]
+var i
+for(i = 0; i < listofaddresses.length; i++){console.log(sentence + " " + listofaddresses[i])}
